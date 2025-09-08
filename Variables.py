edfc487dd483fb68d49b9020a0f84e1c0d8d5bfb
sguerro25 <sebastian41590@gmail.com>
2025-09-08 14:51:35 -0400

    Update Variables.py
    deleted declared variables per instructor request

--- Variables.py
+++ Variables.py
@@ -1,17 +1,17 @@
-mult_x = 5
-mult_y = 6
+mult_x = 
+mult_y = 
 print(mult_x * mult_y)
 
-div_x = 8
-div_y = 3
+div_x = 
+div_y = 
 print(div_x / div_y)
 print(div_x % div_y)
 print(div_x // div_y)
 
-add_x = 4200000
-add_y = 23090
+add_x = 
+add_y = 
 print(add_x + add_y)
 
-sub_x = 4200000
-sub_y = 7633
+sub_x = 
+sub_y = 
 print(sub_x - sub_y)
