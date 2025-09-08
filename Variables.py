269e8f73099f64970e4386d7f0ed0c73b8fd6a74
sguerrero25 <sebastian41590@gmail.com>
2025-09-08 14:45:23 -0400

    Initial commit

---
+++ Variables.py
@@ -0,0 +1,17 @@
+mult_x = 5
+mult_y = 6
+print(mult_x * mult_y)
+
+div_x = 8
+div_y = 3
+print(div_x / div_y)
+print(div_x % div_y)
+print(div_x // div_y)
+
+add_x = 4200000
+add_y = 23090
+print(add_x + add_y)
+
+sub_x = 4200000
+sub_y = 7633
+print(sub_x - sub_y)
